520ef6ccfb7b4736a3a514b4c5cec03da291ec87
Brisack Bertrand <la238814@student.helha.be>
2025-11-20 15:03:38 +0100

    Pour Bastien

--- Modele/Player.cpp
+++ Modele/Player.cpp
@@ -4,39 +4,48 @@
 
 #include "Player.h"
 
+//  Créer un joueur avec un nom par défaut
 Player::Player() {
     setPlayerName("James Adams");
     playerScore[12] = {0};
 }
 
+//  Créer un joueur avec un nom spécifique
 Player::Player(std::string name) {
     setPlayerName(name);
     playerScore[12] = {0};
 }
 
+//  Détruire un joueur
 Player::~Player() {
     // Todo
 }
 
+//  Modifier le nom d'un joueur
 void Player::setPlayerName(std::string name) {
     if (name != "") {
         playerName = name;
     }
 }
 
+// Modifier le score d'une mission du joueur
+// Modification uniquement si meilleur score
 void Player::setPlayerScore(int missionIndex, int newScore) {
     if (newScore > playerScore[missionIndex]) {
         playerScore[missionIndex] = newScore;
     }
 }
 
+//  Récupérer le nom du joueur
 std::string Player::getPlayerName() {
     return playerName;
 }
-int getPlayerScore(int missionIndex) {
+//  Récupérer le score du joueur pour une mission
+int Player::getPlayerScore(int missionIndex) {
     return playerScore[missionIndex];
 }
-int getPlayerTotalScore() {
+//  Récupérer le score total du joueur
+int Player::getPlayerTotalScore() {
     int totalScore = 0;
     for (int i = 0; i < 12; i++) {
         totalScore += playerScore[i];
